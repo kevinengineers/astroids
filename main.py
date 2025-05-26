b6dd3d7e886e24f0a499f79ab799277204504d2f
kevinengineers <kevinengineers@proton.me>
2025-05-26 14:31:17 -0700

    added dt to limit framerate to 60 fps

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        dt = clock.tick(60) / 1000
         screen.fill("black")
         pygame.display.flip()
 
