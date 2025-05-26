1a38ada4bf23ad1970ef8ec32cc48e4543c81630
kevinengineers <kevinengineers@proton.me>
2025-05-26 14:06:01 -0700

    cleaned up main() and removed comments/print statements

--- main.py
+++ main.py
@@ -2,16 +2,9 @@ from constants import *
 import pygame
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: { SCREEN_WIDTH }")
-    print(f"Screen height: { SCREEN_HEIGHT }")
-
     pygame.init()
-    
-    # initalize GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    #create game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,6 +12,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
