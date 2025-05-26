a19d3bd1e80b3d3a4a2f417d2ffdb44905bb77c2
kevinengineers <kevinengineers@proton.me>
2025-05-26 15:43:15 -0700

    added a constant, added a player class and edited main to draw a triangle

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from constants import *
 import pygame
 
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +21,7 @@ def main():
         
         dt = clock.tick(60) / 1000
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
