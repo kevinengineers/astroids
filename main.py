5d9a49c2e2be861ff39ba9448ad97737e6153f2e
kevinengineers <kevinengineers@proton.me>
2025-05-26 18:00:11 -0700

    added asteroid and asteroidField classes and logic, created containers from pygame

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from asteroid import Asteroid
 import pygame
-
 from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
 
-    player = Player(x, y)
+
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
