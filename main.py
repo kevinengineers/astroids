05e1d4420d7a652ce8d843a962db000c35705bac
kevinengineers <kevinengineers@proton.me>
2025-05-26 20:37:32 -0700

    added split method on asteroid to split bigger asteroids into 2 smaller ones

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ def main():
             #check for collisions.
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
                     shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
